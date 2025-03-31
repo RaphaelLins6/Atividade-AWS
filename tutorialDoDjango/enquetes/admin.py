3ab0faef9600c78d5607403b3e8cb4f2d169cf62
Raphael Lins <rapharell06@gmail.com>
2025-03-30 22:27:34 -0300

    Update

--- tutorialDoDjango/enquetes/admin.py
+++ tutorialDoDjango/enquetes/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
+from .models import Question, Choice
 
-from .models import Question
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+    list_per_page = 10
+    date_hierarchy = "pub_date"
+    ordering = ["-pub_date"]
+    actions_on_top = True
+    
+admin.site.register(Question, QuestionAdmin)
